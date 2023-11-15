bcbc8a44794c06a95f4a9c8716f02f02e72aa1ad
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-11-15 12:19:21 +0100

    update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,1 +1,12 @@
-Hello 
+const toggleNav = () => {
+    const openNav = document.getElementsByClassName('openNav')[0];
+    const closeNav = document.getElementsByClassName('closeNav')[0];
+    const navigation = document.getElementsByClassName('navigation')[0];
+
+    //On click open the navigation, hide the openNav and show the closeNav
+    openNav.addEventListener('click', () =>{
+        console.log(1);
+    });
+}
+
+toggleNav();
