ac6266e4d824c2ab71138d7ff50c6806746bcc57
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-11-15 09:44:29 +0100

    Update 1 HomeController, Home blade, and added a route in web.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<x-website-layout>
+    <!-- Header -->
+    <nav>
+        <div>
+            <!-- Logo -->
+            <div>
+                <a href="#"><img src="#" alt="logo-site"></a>
+            </div>
+            <!-- End Logo -->
+            <!-- Navigation -->
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Job</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <!-- End Navigation -->
+            <!-- Login and Post button -->
+            <div>
+                <a href="#">Login/Sign up</a>
+                <a href="#">Post a job</a>
+            </div>
+            <!-- End Login and Post button -->
+        </div>
+    </nav>
+    <!-- End Header -->
+</x-website-layout>
