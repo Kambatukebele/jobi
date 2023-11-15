06a5f650291ec5538b7e3715f7954dca3f3ffdd4
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-11-15 16:24:55 +0100

    update 1 home.js

--- public/js/home.js
+++ public/js/home.js
@@ -5,7 +5,20 @@ const toggleNav = () => {
 
     //On click open the navigation, hide the openNav and show the closeNav
     openNav.addEventListener('click', () =>{
-        console.log(1);
+        openNav.classList.remove('block');
+        openNav.classList.add('hidden');
+        closeNav.classList.remove('hidden');
+        closeNav.classList.add('block');
+        navigation.classList.remove('-ml-[280px]');
+    });
+
+    //On click close the navigation, show the openNav and hide the closeNav
+    closeNav.addEventListener('click', () =>{
+        navigation.classList.add('-ml-[280px]');
+        closeNav.classList.remove('block');
+        closeNav.classList.add('hidden');
+        openNav.classList.remove('hidden');
+        openNav.classList.add('block');
     });
 }
 
